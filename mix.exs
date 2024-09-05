eee61599affa893804fae2738b1a155bf6a5d525
Stephen Newman <steve@newman.digital>
2024-09-05 10:16:50 +0100

    Add docs support.

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule Pento.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:ex_doc, "~> 0.34"}
     ]
   end
 
